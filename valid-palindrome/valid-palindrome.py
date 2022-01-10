c699d48409f5a6725d791978b05dd61aa3a4b16e
Sanjay Shanbhag <95454351+Sanjay-Shan@users.noreply.github.com>
2022-01-09 21:37:58 -0500

    Time: 37 ms, Memory: 15.4 MB - LeetHub

---
+++ valid-palindrome/valid-palindrome.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s=s.lower()
+        s=[j for j in s if j.isalnum()]
+        return s==s[::-1]
+        
