012b5183fe2ba41c4ce3e20ca0ef7b509a7a9bc9
Sanjay Shanbhag <95454351+Sanjay-Shan@users.noreply.github.com>
2022-08-01 00:02:09 -0400

    Time: 76 ms (50.81%), Space: 14.7 MB (35.75%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -3,7 +3,8 @@ class Solution:
         # range of alphabets in ASCII (65-90) & (97-122)
         r=""
         for i in range(len(s)):
-            if (ord(s[i])>=65 and ord(s[i])<=90) or (ord(s[i])>=97 and ord(s[i])<=122) or ((ord(s[i])>=48 and ord(s[i])<=57)):
+            # if (ord(s[i])>=65 and ord(s[i])<=90) or (ord(s[i])>=97 and ord(s[i])<=122) or ((ord(s[i])>=48 and ord(s[i])<=57)):
+            if s[i].isalnum():
                 if (ord(s[i])>=65 and ord(s[i])<=90):
                     r+=s[i].lower()
                 else:
@@ -14,6 +15,7 @@ class Solution:
     # functions used
     # 1. ord() chr()
     # 2. str.lower()
-    # 3. ASCII (A-Z,65-90) (a-z,97-122)
+    # 3. ASCII (A-Z,65-90) (a-z,97-122) (0-9,48-57)
+    # 4. isalnum() could also be used instead of a combination of ord conditions
             
         
