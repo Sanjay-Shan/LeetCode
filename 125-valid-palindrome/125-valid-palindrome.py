152b2460e8b9801ab742a73055a089c683b90e9d
Sanjay Shanbhag <95454351+Sanjay-Shan@users.noreply.github.com>
2022-07-31 22:58:30 -0400

    Time: 91 ms (29.73%), Space: 14.7 MB (42.09%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # range of alphabets in ASCII (65-90) & (97-122)
+        r=""
+        for i in range(len(s)):
+            if (ord(s[i])>=65 and ord(s[i])<=90) or (ord(s[i])>=97 and ord(s[i])<=122) or ((ord(s[i])>=48 and ord(s[i])<=57)):
+                if (ord(s[i])>=65 and ord(s[i])<=90):
+                    r+=s[i].lower()
+                else:
+                    r+=s[i]
+        print(r[::-1],r)
+        return r[::-1]==r
+    
+    # functions used
+    # 1. ord() chr()
+    # 2. str.lower()
+    # 3. ASCII (A-Z,65-90) (a-z,97-122)
+            
+        
